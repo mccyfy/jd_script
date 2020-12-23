f8eb3cb6b5428259b8c06524c7ecf2892f4178ac
mccyfy <35087878+mccyfy@users.noreply.github.com>
2020-12-23 09:14:31 +0800

    Add files via upload

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -8,14 +8,13 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-    'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号二的好友shareCode，不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号二的好友shareCode，不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号二的好友shareCode，不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号二的好友shareCode，不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'P04z54XCjVWnYaS5mlRWjmmlTsA@P04z54XCjVWnYaS5m9cZ2b5iXkcw5iRj8NvNdQ@P04z54XCjVWnYaS5m9cZ2f43HVLwafQDX2_87k@P04z54XCjVWnYaS5m9cZ2f93SoekdZESEmxoPM@P04z54XCjVWnYaS5m9cZ2b71H5CkXdG2iHu_ZQ',//账号二的好友shareCode，不同好友中间用@符号隔开
- 
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号一的好友shareCode,不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  'AGBH61xm2K9Gj1_qP_0Tew==',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有东东萌宠互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
